d43b0a83a663146d8e3ada18e2c6f4a2d91bc1e7
mn297 <mn297@users.noreply.github.com>
2026-03-29 23:22:19 -0400

    Enhance Ubuntu setup script with Docker and NVIDIA container toolkit installation; add new basics.sh script for Isaac ROS package management

--- ubuntu/setup.sh
+++ ubuntu/setup.sh
@@ -82,6 +82,78 @@ EOF
 
 sudo keyd reload
 
+# docker
+sudo apt remove $(dpkg --get-selections docker.io docker-compose docker-compose-v2 docker-doc podman-docker containerd runc | cut -f1) || true
+
+sudo apt update
+sudo apt install -y ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
+Types: deb
+URIs: https://download.docker.com/linux/ubuntu
+Suites: $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}")
+Components: stable
+Architectures: $(dpkg --print-architecture)
+Signed-By: /etc/apt/keyrings/docker.asc
+EOF
+
+sudo apt update
+sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# nvidia container toolkit
+sudo apt-get update && sudo apt-get install -y --no-install-recommends \
+  ca-certificates \
+  curl \
+  gnupg2
+
+curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | \
+  sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg
+
+curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list | \
+  sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
+  sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
+
+sudo apt-get update
+
+export NVIDIA_CONTAINER_TOOLKIT_VERSION=1.19.0-1
+sudo apt-get install -y \
+  nvidia-container-toolkit=${NVIDIA_CONTAINER_TOOLKIT_VERSION} \
+  nvidia-container-toolkit-base=${NVIDIA_CONTAINER_TOOLKIT_VERSION} \
+  libnvidia-container-tools=${NVIDIA_CONTAINER_TOOLKIT_VERSION} \
+  libnvidia-container1=${NVIDIA_CONTAINER_TOOLKIT_VERSION}
+
+sudo nvidia-ctk runtime configure --runtime=docker
+sudo systemctl restart docker
+
+
+# isaac ros
+mkdir -p ~/workspaces/isaac_ros-dev/src
+echo 'export ISAAC_ROS_WS="${ISAAC_ROS_WS:-${HOME}/workspaces/isaac_ros-dev/}"' >> ~/.bashrc
+source ~/.bashrc
+
+sudo apt update && sudo apt install -y locales
+sudo locale-gen en_US en_US.UTF-8
+sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
+export LANG=en_US.UTF-8
+
+sudo apt update && sudo apt install -y curl gnupg software-properties-common
+sudo add-apt-repository universe
+
+k="/usr/share/keyrings/nvidia-isaac-ros.gpg"
+curl -fsSL https://isaac.download.nvidia.com/isaac-ros/repos.key | sudo gpg --dearmor | sudo tee -a $k > /dev/null
+f="/etc/apt/sources.list.d/nvidia-isaac-ros.list"
+sudo touch $f
+s="deb [signed-by=$k] https://isaac.download.nvidia.com/isaac-ros/release-4.3 noble main"
+grep -qxF "$s" $f || echo "$s" | sudo tee -a $f
+sudo apt-get update
+
+sudo apt-get install -y isaac-ros-cli
+
+
+
 # Install CUDA
 # wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-keyring_1.1-1_all.deb
 # sudo dpkg -i cuda-keyring_1.1-1_all.deb
@@ -91,15 +163,15 @@ sudo keyd reload
 
 # Download Isaac Sim
 # check if $HOME/isaacsim exists
-if [ ! -d "$HOME/isaacsim" ]; then
-	echo "Isaac Sim not found, downloading..."
-	ISAACSIM_URL="https://download.isaacsim.omniverse.nvidia.com/isaac-sim-standalone-5.1.0-linux-x86_64.zip"
-	ISAACSIM_ZIP="$HOME/isaacsim.zip"
-	ISAACSIM_DIR="$HOME/isaacsim"
-	wget -O "$ISAACSIM_ZIP" "$ISAACSIM_URL"
-	mkdir -p "$ISAACSIM_DIR"
-	unzip -o "$ISAACSIM_ZIP" -d "$ISAACSIM_DIR"
-	echo "Isaac Sim downloaded and extracted to $ISAACSIM_DIR"
-else
-	echo "Isaac Sim already exists, skipping download..."
-fi
+# if [ ! -d "$HOME/isaacsim" ]; then
+# 	echo "Isaac Sim not found, downloading..."
+# 	ISAACSIM_URL="https://download.isaacsim.omniverse.nvidia.com/isaac-sim-standalone-5.1.0-linux-x86_64.zip"
+# 	ISAACSIM_ZIP="$HOME/isaacsim.zip"
+# 	ISAACSIM_DIR="$HOME/isaacsim"
+# 	wget -O "$ISAACSIM_ZIP" "$ISAACSIM_URL"
+# 	mkdir -p "$ISAACSIM_DIR"
+# 	unzip -o "$ISAACSIM_ZIP" -d "$ISAACSIM_DIR"
+# 	echo "Isaac Sim downloaded and extracted to $ISAACSIM_DIR"
+# else
+# 	echo "Isaac Sim already exists, skipping download..."
+# fi
